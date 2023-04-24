835d6c51b20cca54c03102a6a158f0da3c3410c4
weijia.wang1 <weijia.wang1@nio.com>
2023-04-24 11:22:58 +0800

    page init

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,25 @@
-declare function main(): void;
+import 'reflect-metadata';
+interface IConfig {
+    key?: string;
+    enableNULL?: boolean;
+    formatValue?: (value: any, baseValue: any) => any;
+}
+interface ISingleConfig extends IConfig {
+    type?: 'single';
+}
+interface IObjectConfig extends IConfig {
+    type: 'object';
+    objectItem: any;
+}
+interface IArrayConfig extends IConfig {
+    type: 'array';
+    arrayItem: any;
+}
+interface IDateConfig extends IConfig {
+    type: 'date';
+    formatStr: string;
+}
+type TConfig = ISingleConfig | IArrayConfig | IObjectConfig | IDateConfig;
+export declare function ModelCol(config: TConfig): (target: any, propertyKey: any) => void;
+export declare function ModelEnter(): (constructor: any, _?: any) => any;
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,116 @@
-function main() {
-  console.log("wwj");
+import 'reflect-metadata';
+import * as dayjs from 'dayjs';
+
+const ClassBaseModelKey = Symbol('class');
+
+interface IConfig {
+  // 数据源 key，用于对应原始数据，不传默认与 对象字段相同
+  key?: string;
+  // 是否可以 赋值 null
+  enableNULL?: boolean;
+  // 格式化 数据
+  formatValue?: (value: any, baseValue: any) => any;
+}
+
+// 普通类型
+interface ISingleConfig extends IConfig {
+  type?: 'single';
+}
+
+// 对象类型
+interface IObjectConfig extends IConfig {
+  type: 'object';
+  objectItem: any;
+}
+
+// 数组类型
+interface IArrayConfig extends IConfig {
+  type: 'array';
+  arrayItem: any;
+}
+
+// 时间类型
+interface IDateConfig extends IConfig {
+  type: 'date';
+  formatStr: string;
+}
+
+type TConfig = ISingleConfig | IArrayConfig | IObjectConfig | IDateConfig;
+
+export function ModelCol(config: TConfig) {
+  return function (target: any, propertyKey: any) {
+    if (!target._baseKeys) {
+      target._baseKeys = [];
+    }
+    target._baseKeys.push(propertyKey);
+
+    const newFun = Reflect.metadata(ClassBaseModelKey, config);
+    newFun(target, propertyKey);
+  };
+}
+
+export function ModelEnter() {
+  return function (constructor: any, _?: any) {
+    return class extends constructor {
+      constructor(props?: any) {
+        super(props);
+
+        this._baseProse_ = props;
+
+        this._baseKeys.forEach((propsKey: string) => {
+          const config: TConfig = Reflect.getMetadata(ClassBaseModelKey, this, propsKey);
+          const key = config.key || propsKey;
+          if (props) {
+            const formatValue = config.formatValue;
+            // 获取原始 数据
+            let value = props[key];
+            // 如果有格式化方法，格式化数据
+            if (formatValue) {
+              value = formatValue(value, props);
+            }
+            if (value === null) {
+              if (config.enableNULL) {
+                // 如果数据是 null，并且允许数据是 null，那么赋值 null
+                this[propsKey] = value;
+                return;
+              }
+              console.warn(`[key:${key}] is null`);
+              return;
+            }
+
+            if (value !== undefined) {
+              try {
+                if (!config.type || config.type === 'single') {
+                  this[propsKey] = value;
+                  return;
+                } else if (config.type === 'array') {
+                  const tempConfig: IArrayConfig = config;
+                  this[propsKey] = (value || []).map((arrayItem: any) => {
+                    return new tempConfig.arrayItem(arrayItem);
+                  });
+                  return;
+                } else if (config.type === 'object') {
+                  const tempConfig: IObjectConfig = config;
+                  this[propsKey] = new tempConfig.objectItem(value);
+                  return;
+                } else if (config.type === 'date') {
+                  const tempConfig: IDateConfig = config;
+                  this[propsKey] = dayjs(value).format(tempConfig.formatStr);
+                  return;
+                } else {
+                  this[propsKey] = value;
+                  return;
+                }
+              } catch (error) {
+                console.log(`init [${key}] error with value [${value}]`);
+                console.error(error);
+              }
+            }
+          } else {
+            console.warn('model init no props');
+          }
+        });
+      }
+    } as any;
+  };
 }
