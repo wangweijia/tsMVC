26f3ac75ca47dbaf214715f9ecd5089688681529
weijia.wang1 <weijia.wang1@nio.com>
2023-10-07 16:07:19 +0800

    chore: bump to 2.0.25

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,22 +1,22 @@
 import 'reflect-metadata';
 import { TConfig } from './types/modelConfig';
-declare class ModelBaseClass2 {
-    [k: string]: any;
+declare class ModelBaseClassRoot {
     constructor(...args: any[]);
+    _baseProse_: any;
     _init_?(...p: any): void;
     _initUUID_?(v?: string): string;
     _OTD_?(): {};
     static InitWithList?(items: Array<any>): Array<any>;
 }
-export declare class ModelBaseClass extends ModelBaseClass2 {
+export declare class ModelBaseClass extends ModelBaseClassRoot {
     _OTD_(): {};
     static InitWithList<T>(items: Array<any>): Array<T>;
 }
-type TClass = typeof ModelBaseClass;
+type TClassBase = typeof ModelBaseClass;
 export declare function ModelCol(config: TConfig): (target: any, propertyKey: any) => void;
 export declare function ModelAutoUUID(): (target: any, propertyKey: any) => void;
 interface IClassOpt {
     _debugger_?: boolean;
 }
-export declare function ModelEnter(opt?: IClassOpt): <T extends typeof ModelBaseClass2>(constructor: T, _?: any) => T & typeof ModelBaseClass;
+export declare function ModelEnter(opt?: IClassOpt): <T extends typeof ModelBaseClassRoot>(constructor: T, _?: any) => T & typeof ModelBaseClass;
 export {};
